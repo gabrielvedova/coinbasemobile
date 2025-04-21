6b839f44b7de0467d11a5555bafbd4737ec48931
gabrielvedova <bielvedova@gmail.com>
2025-04-21 18:33:00 -0300

    fix(Reports): wrap report text in SafeAreaView for improved layout and styling

--- CoinBase/src/views/Reports.js
+++ CoinBase/src/views/Reports.js
@@ -1,6 +1,18 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, StyleSheet, SafeAreaView } from "react-native";
 
 export default (props) => {
-  return <Text>Relatório de gastos</Text>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Relatório de gastos</Text>
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
